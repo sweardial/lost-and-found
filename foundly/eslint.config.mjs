ba244312a8583ca097213abb64e282eacae622c7
Lleonore <chesnokovdmytro@gmail.com>
2025-04-17 22:53:17 -0400

    add eslit rule

--- foundly/eslint.config.mjs
+++ foundly/eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    extends: ["next"],
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
